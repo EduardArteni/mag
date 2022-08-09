54d2d315eafc84bd3a920dddb5010433840fed43
EduardArteni <eduard.augustin.arteni@gmail.com>
2022-08-09 22:27:48 +0300

    solved cors

--- src/main/java/com/arteni/mag/Controllers/UserController.java
+++ src/main/java/com/arteni/mag/Controllers/UserController.java
@@ -2,6 +2,8 @@ package com.arteni.mag.Controllers;
 
 import com.arteni.mag.DataBaseConnection;
 import com.arteni.mag.Models.User;
+import com.arteni.mag.Models.request.UserManagementResponse;
+import org.postgresql.util.PSQLException;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.*;
@@ -10,14 +12,44 @@ import java.sql.*;
 @RestController
 public class UserController {
 
+    //TODO move to DAO
+    public static String ERROR_MESSAGE_UNIQUE_USER = "user_username_key";
+
+    @CrossOrigin(origins = "http://127.0.0.1:5500/")
     @RequestMapping(value = "/user", method = RequestMethod.GET)
     public User getUser() {
-        return new User();
+        return new User(122, "Radu Rus", "Bulevardul Capitan numaistiucum");
     }
 
+    /*
+    @CrossOrigin(origins = "http://localhost:5500")
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public User getUserById(@RequestParam(value = "id") int id) {
+        try {
+            PreparedStatement createUserStatement = DataBaseConnection.connection.prepareStatement("");
+            createUserStatement.setString(1, username);
+            createUserStatement.setString(2, password);
+            ResultSet resultSet = createUserStatement.executeQuery();
+            resultSet.next();
+            id = resultSet.getInt(1);
+            userManagementResponse.responseCode = "0";
+            userManagementResponse.message = "user created successfully";
+        } catch (PSQLException e) {
+
+        } catch (Exception e) {
+        }
+        return new User(1,"test","test");
+    }
+     */
+    @CrossOrigin(origins = "http://127.0.0.1:5500/")
     @RequestMapping(value = "/user", method = RequestMethod.POST)
-    public User createUser(@RequestParam(value = "username") String username, @RequestParam(value = "password") String password) {
+    public UserManagementResponse createUser(@RequestParam(value = "username") String username, @RequestParam(value = "password") String password) {
         User createdUser = new User();
+        UserManagementResponse userManagementResponse = new UserManagementResponse();
+        userManagementResponse.user = createdUser;
+
+        //TODO move to DAO
+
         int id = 0;
         try {
             PreparedStatement createUserStatement = DataBaseConnection.connection.prepareStatement("INSERT INTO public.\"user\" (username, password) VALUES (?, ?) RETURNING id;");
@@ -26,6 +58,21 @@ public class UserController {
             ResultSet resultSet = createUserStatement.executeQuery();
             resultSet.next();
             id = resultSet.getInt(1);
+            userManagementResponse.responseCode = "0";
+            userManagementResponse.message = "user created successfully";
+        } catch (PSQLException e) {
+
+            if (e.getMessage().contains(ERROR_MESSAGE_UNIQUE_USER)) {
+                userManagementResponse.responseCode = "501";
+                userManagementResponse.message = "username already exists";
+
+                System.out.println("USERNAME ALREADY EXISTS");
+            } else {
+                e.printStackTrace();
+                userManagementResponse.responseCode = "500";
+                userManagementResponse.message = "UNKNOWN ERROR";
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -34,7 +81,7 @@ public class UserController {
         createdUser.password = password;
         createdUser.id = id;
 
-        return createdUser;
+        return userManagementResponse;
     }
 
     /*
---
+++ src/main/java/com/arteni/mag/Models/request/UserManagementResponse.java
@@ -0,0 +1,12 @@
+package com.arteni.mag.Models.request;
+
+import com.arteni.mag.Models.User;
+
+public class UserManagementResponse {
+
+    public User user;
+    public String message;
+    //TODO use enums
+    public String responseCode;
+
+}
