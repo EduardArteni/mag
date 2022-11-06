183ccffade5c8252b92c0131f02068588fb3acfe
fedalfred <fed.tudor@gmail.com>
2022-11-06 10:29:11 +0200

    Update User.java
    Used the best practices with setters and getters

--- src/main/java/com/arteni/mag/Models/User.java
+++ src/main/java/com/arteni/mag/Models/User.java
@@ -1,9 +1,9 @@
 package com.arteni.mag.Models;
 
 public class User {
-    public int id;
-    public String username;
-    public String password;
+    private int id;
+    private String username;
+    private String password;
 
     public User(int id, String username, String password) {
         this.id = id;
@@ -11,6 +11,35 @@ public class User {
         this.password = password;
     }
 
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
     public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
