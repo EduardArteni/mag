2ee55290e9bf90aa58f22877723ff2b932986a3a
fedalfred <fed.tudor@gmail.com>
2022-11-06 10:27:01 +0200

    Update db_updates.sql
    Added default value plus some test data.

--- src/main/resources/db-schema/db_updates.sql
+++ src/main/resources/db-schema/db_updates.sql
@@ -1,3 +1,8 @@
 -- added creared_date column to users table
 ALTER TABLE public."user"
 ADD COLUMN created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+
+-- add some test data
+INSERT INTO public.product(
+	name, description, "SKU", category, price, created_at)
+	VALUES ('TV', 'TV Samsung', 'TVSMSNG-GEN1', 'ELECTRO', 1000, now());
