dd788be780ceeab95447423c778b07ece153de68
fedalfred <fed.tudor@gmail.com>
2022-11-06 11:08:38 +0200

    Added find by SKU feature
    Added find by SKU feature

--- src/main/java/com/arteni/mag/dao/ProductDAO.java
+++ src/main/java/com/arteni/mag/dao/ProductDAO.java
@@ -14,7 +14,29 @@ public class ProductDAO {
 
 
     public Product getProductBySKU(String SKU) {
-        return new Product();
+        System.out.println("ProductDAO.getProductBySKU");
+        System.out.println("SKU = " + SKU);
+
+        Product product = new Product();
+        try {
+            PreparedStatement preparedStatement = DataBaseConnection.connection.prepareStatement("SELECT id, name, description, \"SKU\", category, price, created_at FROM public.product WHERE \"SKU\"  = ?;");
+            System.out.println(preparedStatement);
+            preparedStatement.setString(1,SKU);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                product.id = resultSet.getInt(1);
+                product.name = resultSet.getString(2);
+                product.description = resultSet.getString(3);
+                product.SKU = resultSet.getString(4);
+                product.category = resultSet.getString(5);
+                product.price = resultSet.getDouble(6);
+                product.created_at = resultSet.getTimestamp(7);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return product;
     }
 
     public ArrayList<Product> getProductsByCategory(String category) {
