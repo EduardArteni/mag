46a3211f6b559aec99aca1b0c74a764d78c19d73
fedalfred <fed.tudor@gmail.com>
2022-11-12 19:43:37 +0200

    added test classes for controllers

---
+++ src/test/java/com/arteni/mag/Controllers/ProductControllerTest.java
@@ -0,0 +1,20 @@
+package com.arteni.mag.Controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductControllerTest {
+
+    @Test
+    void getProductsByName() {
+    }
+
+    @Test
+    void getProductsBySKU() {
+    }
+
+    @Test
+    void getProductById() {
+    }
+}
---
+++ src/test/java/com/arteni/mag/Controllers/UserControllerTest.java
@@ -0,0 +1,16 @@
+package com.arteni.mag.Controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+    @Test
+    void loginUser() {
+    }
+
+    @Test
+    void createUser() {
+    }
+}
